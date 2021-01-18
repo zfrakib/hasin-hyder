4ebc4ff0a69d5e0f5e24900e1ccbe6e75e211c83
zfrakib <zannatfr.org@gmail.com>
2021-01-18 21:31:50 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,19 @@
+// cost of Phone Calculation
+var phonePrice = 13000;
+var numberOfPhone = 18;
+var totalCost = phonePrice * numberOfPhone;
+
+console.log('Total Cost Of Phone',totalCost,'taka');
+
+// egg circle calculation
+var eggRadius = 7;
+const pi = 3.14159;
+var areaOfCircle = pi * ( eggRadius * eggRadius );
+console.log("Area of circle is",areaOfCircle,"sqr inch");
+
+
+// usd to bdt calculator
+var usdToBdt = 84.2;
+var bdt = 3870;
+var usd = bdt / usdToBdt;
+console.log("$",usd);
